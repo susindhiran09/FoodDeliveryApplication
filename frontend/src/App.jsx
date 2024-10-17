8a5c2cdcefd9251d2a53ced19166acc8063387e9
susindhiran km <susindhiran9@gmail.com>
2024-10-17 11:48:16 +0530

    initialize

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar></Navbar>
+      
+    </div>
+  )
+}
+
+export default App
